3446f43a51b5795846fe85497ae5dd2e24a2611e
Ryan Kowalewski <rjkowalewski@gmail.com>
2017-06-23 14:17:45 +0100

    Added Independent.co.uk news ticker in WindowTitle

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,14 @@
 function prompt
 {
     # Set prompt options
-    $host.UI.RawUI.WindowTitle = (Get-Location).ToString() + " ~ " + $weatherString
+    $IndyFeedCurrentItem = Get-Content $PSScriptRoot\IndyFeed.txt
+    $host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $weatherString + " ~ " + $IndyFeedCurrentItem
     $currentDir = Split-Path (Get-Location) -Leaf
     "[$env:USERNAME] $currentDir>"
 }
 
-
 # Set initial working directory
 Set-Location E:\Cher\Scripts
-
-# Get weather information from Yahoo
-$URI = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where  text='{0}, {1}') and u='c'&format=json&env=store://datatables.org/alltableswithkeys"  -f 'Southampton','UK'
-$Data = Invoke-RestMethod -Uri  $URI
-$weatherString = $Data.query.results.channel.item.forecast | ?{$_.date -eq (Get-Date).ToString('dd MMM yyyy')}
-$weatherString = $weatherString.high + "$([char]0x00B0) " + $weatherString.text
 
 # Configure shell theme/size
 $host.UI.RawUI.ForegroundColor = "White"
@@ -33,3 +27,31 @@ Clear-Host
 # On previous versions of Windows you need to install it from https://github.com/lzybkr/PSReadLine
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
+# Get weather information from Yahoo
+$weatherCity = "Southampton"
+$weatherCountry = "UK"
+$URI = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='{0}, {1}')`
+ and u='c'&format=json&env=store://datatables.org/alltableswithkeys"  -f $weatherCity, $weatherCountry
+
+$Data = Invoke-RestMethod -Uri  $URI
+$weatherString = $Data.query.results.channel.item.forecast | ?{$_.date -eq (Get-Date).ToString('dd MMM yyyy')}
+$weatherString = $weatherString.high + "$([char]0x00B0) " + $weatherString.text
+
+# Get news feed from Independent.co.uk
+[xml]$IndyRSS = Invoke-WebRequest -Uri 'http://www.independent.co.uk/rss'
+$IndyFeed = $IndyRSS.rss.channel
+$IndyFeedCurrentItem = ""
+$Timer = New-Object System.Timers.Timer
+$Timer.Interval = 30000
+$TimerAction = {
+    $i++
+    If ($i -lt ($IndyFeed.Item.Count))
+    {
+        $IndyFeedCurrentItem = $IndyFeed.Item[$i].title
+        $Host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $weatherString + " ~ " + $IndyFeedCurrentItem
+        $IndyFeedCurrentItem | Out-File $PSScriptRoot\IndyFeed.txt
+    }
+}
+Register-ObjectEvent -InputObject $Timer -EventName Elapsed -SourceIdentifier IndyFeed -Action $TimerAction | Out-Null
+$Timer.Start()
