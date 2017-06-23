ab2901c5ae134b1ee9605e4810ec4f58468fa570
Ryan Kowalewski <rjkowalewski@gmail.com>
2017-06-23 12:21:58 +0100

    Added Yahoo Weather to title bar.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,20 @@
 function prompt
 {
     # Set prompt options
-    $host.UI.RawUI.WindowTitle = Get-Location
+    $host.UI.RawUI.WindowTitle = (Get-Location).ToString() + " ~ " + $weatherString
     $currentDir = Split-Path (Get-Location) -Leaf
     "[$env:USERNAME] $currentDir>"
 }
+
+
+# Set initial working directory
+Set-Location E:\Cher\Scripts
+
+# Get weather information from Yahoo
+$URI = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where  text='{0}, {1}') and u='c'&format=json&env=store://datatables.org/alltableswithkeys"  -f 'Southampton','UK'
+$Data = Invoke-RestMethod -Uri  $URI
+$weatherString = $Data.query.results.channel.item.forecast | ?{$_.date -eq (Get-Date).ToString('dd MMM yyyy')}
+$weatherString = $weatherString.high + "$([char]0x00B0) " + $weatherString.text
 
 # Configure shell theme/size
 $host.UI.RawUI.ForegroundColor = "White"
@@ -23,6 +33,3 @@ Clear-Host
 # On previous versions of Windows you need to install it from https://github.com/lzybkr/PSReadLine
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-
-# Set initial working directory
-Set-Location E:\Cher\Scripts
