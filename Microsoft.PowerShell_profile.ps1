6ffd2ee8e7324d4431ad25f3633c0197ca769358
Ryan Kowalewski <rjkowalewski@gmail.com>
2017-06-25 13:11:34 +0100

    Script formatting and added regions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
+#region globals
+$profileInitialDir = 'E:\Cher\Scripts'
+$weatherCity = "Salisbury"
+$weatherCountry = "UK"
+#endregion
+
+#region functions
 function prompt
 {
     # Set prompt options
@@ -24,9 +31,13 @@ function New-IndyFeedCurrentItem
     $Item | Out-File $PSScriptRoot\IndyFeed.txt
     return $Item
 }
+#endregion
 
 # Set initial working directory
-Set-Location E:\Cher\Scripts
+If (Test-Path $profileInitialDir)
+{
+    Set-Location $profileInitialDir
+}
 
 # Configure shell theme/size
 $host.UI.RawUI.ForegroundColor = "White"
@@ -47,32 +58,29 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 
 # Get weather information from Yahoo
-$weatherCity = "Southampton"
-$weatherCountry = "UK"
-$URI = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='{0}, {1}')`
+$uri = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='{0}, {1}')`
  and u='c'&format=json&env=store://datatables.org/alltableswithkeys"  -f $weatherCity, $weatherCountry
-
-$Data = Invoke-RestMethod -Uri  $URI
-$weatherString = $Data.query.results.channel.item.forecast | ?{$_.date -eq (Get-Date).ToString('dd MMM yyyy')}
+$data = Invoke-RestMethod -Uri  $uri
+$weatherString = $data.query.results.channel.item.forecast | ?{$_.date -eq (Get-Date).ToString('dd MMM yyyy')}
 $weatherString = $weatherString.high + "$([char]0x00B0) " + $weatherString.text
 
 # Get news feed from Independent.co.uk
 $i = 0
 Remove-Item -Path $PSScriptRoot\IndyFeed.* -Force
-[xml]$IndyRSS = Invoke-WebRequest -Uri 'http://www.independent.co.uk/news/uk/rss'
-$IndyRSS.rss.channel | Export-Clixml $PSScriptRoot\IndyFeed.xml
-$IndyFeed = Import-Clixml $PSScriptRoot\IndyFeed.xml
+[xml]$indyRSS = Invoke-WebRequest -Uri 'http://www.independent.co.uk/news/uk/rss'
+$indyRSS.rss.channel | Export-Clixml $PSScriptRoot\IndyFeed.xml
+$indyFeed = Import-Clixml $PSScriptRoot\IndyFeed.xml
 
 # Set background task to loop through RSS feed
-$Timer = New-Object System.Timers.Timer
-$Timer.Interval = 30000
-$TimerAction = {
-    If ($i -lt ($IndyFeed.Item.Count))
+$timer = New-Object System.Timers.Timer
+$timer.Interval = 30000
+$timerAction = {
+    If ($i -lt ($indyFeed.Item.Count))
     {
-        $IndyFeedCurrentItem = New-IndyFeedCurrentItem $IndyFeed.Item[$i].title
-        $Host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $weatherString + " ~ " + $IndyFeedCurrentItem
+        $indyFeedCurrentItem = New-IndyFeedCurrentItem $indyFeed.Item[$i].title
+        $host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $weatherString + " ~ " + $indyFeedCurrentItem
         $i++
     }
 }
-Register-ObjectEvent -InputObject $Timer -EventName Elapsed -SourceIdentifier IndyFeed -Action $TimerAction | Out-Null
-$Timer.Start()
+Register-ObjectEvent -InputObject $timer -EventName Elapsed -SourceIdentifier IndyFeed -Action $timerAction | Out-Null
+$timer.Start()
