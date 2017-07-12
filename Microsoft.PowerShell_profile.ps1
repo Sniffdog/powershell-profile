5e9f7165a02647db13eab2b396968d8a24425cb7
Ryan Kowalewski <rjkowalewski@gmail.com>
2017-07-12 14:35:28 +0100

    general code styling and re-arranging

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,21 @@
-#region globals
-$profileInitialDir = 'E:\Cher\Scripts'
-$RSSUri = 'http://www.independent.co.uk/news/uk/rss'
-$weatherCity = 'Salisbury'
-$weatherCountry = 'UK'
-#endregion
+<#
+    .SYNOPSIS
+    Custom PowerShell profile with some Unix like additions
+#>
+
 
 #region functions
-function prompt
-{
+function prompt {
     # Set prompt options
     $IndyFeedCurrentItem = Get-Content $PSScriptRoot\RSSFeed.txt
-    $host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $weatherString + " ~ " + $IndyFeedCurrentItem
-    $currentDir = Split-Path (Get-Location) -Leaf
+    $Host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $WeatherString + " ~ " + $IndyFeedCurrentItem
+    $CurrentDir = Split-Path (Get-Location) -Leaf
     Write-Host "[$env:USERNAME] " -NoNewline -ForegroundColor Cyan
-    "$currentDir>"
+    "$CurrentDir>"
 }
 
-function New-RSSFeedCurrentItem
-{
+
+function New-RSSFeedCurrentItem {
     # Format RSS Item and store in temp file
     param(
         [parameter(mandatory=$true)]
@@ -29,45 +27,68 @@ function New-RSSFeedCurrentItem
                   -replace "&quot;", '"'`
                   -replace "&amp;", "&"
     $Item | Out-File $PSScriptRoot\RSSFeed.txt
-    return $Item
+    $Item
 }
 
-function Set-ConsoleTheme
-{
-    Param(
+
+function Set-ConsoleTheme {
+    param(
         [Parameter(Mandatory = $true, ParameterSetName = 'WindowsDefault')] 
         [switch]$WindowsDefault,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'DarkTheme')]
         [switch]$DarkTheme
     )
-    If ($DarkTheme)
-    {
+    if ($DarkTheme) {
         # Set ColorTable04 (DarkRed)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable04' -Value '0x002b39c0' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable04' -Value '0x002b39c0' -Type DWord
         # Set ColorTable05 (DarkMagenta)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable05' -Value '0x00222222' -Type DWord
-        Set-ItemProperty HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe -Name 'ColorTable05' -Value '0x00222222' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable05' -Value '0x00222222' -Type DWord
+        $ItemProps = @{
+            Path = "HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe"
+            Name = "ColorTable05"
+            Value = "0x00222222"
+            Type = "DWord"
+        }
+        Set-ItemProperty @ItemProps
         # Set ColorTable06 (DarkYellow)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable06' -Value '0x00fc4f1' -Type DWord
-        Set-ItemProperty HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe -Name 'ColorTable06' -Value '0x00fc4f1' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable06' -Value '0x00fc4f1' -Type DWord
+        $ItemProps = @{
+            Path = "HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe"
+            Name = "ColorTable06"
+            Value = "0x00fc4f1"
+            Type = "DWord"
+        }
+        Set-ItemProperty @ItemProps
         # Set ColorTable11 (Cyan)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable11' -Value '0x00b98029' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable11' -Value '0x00b98029' -Type DWord
     }
-    If ($WindowsDefault)
-    {
+    if ($WindowsDefault) {
         # Set ColorTable04 (DarkRed)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable04' -Value '0x00000080' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable04' -Value '0x00000080' -Type DWord
         # Set ColorTable05 (DarkMagenta)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable05' -Value '0x00800080' -Type DWord
-        Set-ItemProperty HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe -Name 'ColorTable05' -Value '0x00562401' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable05' -Value '0x00562401' -Type DWord
+        $ItemProps = @{
+            Path = "HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe"
+            Name = "ColorTable05"
+            Value = "0x00562401"
+            Type = "DWord"
+        }
+        Set-ItemProperty @ItemProps
         # Set ColorTable06 (DarkYellow)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable06' -Value '0x00008080' -Type DWord
-        Set-ItemProperty HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe -Name 'ColorTable06' -Value '0x00f0edee' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable06' -Value '0x00008080' -Type DWord
+        $ItemProps = @{
+            Path = "HKCU:\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe"
+            Name = "ColorTable06"
+            Value = "0x00f0edee"
+            Type = "DWord"
+        }
+        Set-ItemProperty @ItemProps
         # Set ColorTable11 (Cyan)
-        Set-ItemProperty HKCU:\Console -Name 'ColorTable11' -Value '0x00ffff00' -Type DWord
+        Set-ItemProperty -Path HKCU:\Console -Name 'ColorTable11' -Value '0x00ffff00' -Type DWord
     }
 }
+
 
 function ls 
 {
@@ -106,6 +127,12 @@ function ls
 }
 #endregion
 
+
+$profileInitialDir = 'E:\Cher\Scripts'
+$RSSUri = 'http://www.independent.co.uk/news/uk/rss'
+$weatherCity = 'Salisbury'
+$weatherCountry = 'UK'
+
 # Set initial working directory
 If (Test-Path $profileInitialDir)
 {
@@ -122,7 +149,6 @@ $host.PrivateData.WarningForegroundColor = 'DarkYellow'
 $host.PrivateData.ErrorForegroundColor = 'DarkRed'
 $host.PrivateData.WarningBackgroundColor = 'DarkMagenta'
 $host.PrivateData.ErrorBackgroundColor = 'DarkMagenta'
-
 $hostBufferSize = $host.UI.RawUI.BufferSize
 $hostBufferSize.Width = 150
 $hostBufferSize.Height = 5000
@@ -141,29 +167,49 @@ Set-PSReadlineOption -TokenKind Variable -ForegroundColor DarkRed
 Set-PSReadlineOption -TokenKind Command -ForegroundColor DarkYellow
 
 # Get weather information from Yahoo
-$uri = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='{0}, {1}')`
- and u='c'&format=json&env=store://datatables.org/alltableswithkeys"  -f $weatherCity, $weatherCountry
-$data = Invoke-RestMethod -Uri  $uri
-$weatherString = $data.query.results.channel.item.forecast | ?{$_.date -eq (Get-Date).ToString('dd MMM yyyy')}
-$weatherString = $weatherString.high + "$([char]0x00B0) " + $weatherString.text
+$WeatherUri = ("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in " +
+               "(select woeid from geo.places(1) where text='{0}, {1}') and " +
+               "u='c'&format=json&env=store://datatables.org/alltableswithkeys") -f $WeatherCity, $WeatherCountry
+$WeatherData = Invoke-RestMethod -Uri $WeatherUri
+$WeatherString = $WeatherData.query.results.channel.item.forecast | Where-Object {
+    $_.date -eq (Get-Date).ToString('dd MMM yyyy')
+}
+$WeatherString = $WeatherString.high + "$([char]0x00B0) " + $WeatherString.text
 
-# Get news feed from RSS url
+# Get news feed from RSS url and display in Window title bar
 $i = 0
 Remove-Item -Path $PSScriptRoot\RSSFeed.* -Force
 [xml]$RSS = Invoke-WebRequest -Uri $RSSUri
 $RSS.rss.channel | Export-Clixml $PSScriptRoot\RSSFeed.xml
 $RSSFeed = Import-Clixml $PSScriptRoot\RSSFeed.xml
+$IndyFeedCurrentItem = New-RSSFeedCurrentItem $RSSFeed.Item[$i].title
+$Host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $WeatherString + " ~ " + $IndyFeedCurrentItem
+$i++
 
-# Set background task to loop through RSS feed
-$timer = New-Object System.Timers.Timer
-$timer.Interval = 30000
-$timerAction = {
-    If ($i -lt ($RSSFeed.Item.Count))
-    {
-        $indyFeedCurrentItem = New-RSSFeedCurrentItem $RSSFeed.Item[$i].title
-        $host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $weatherString + " ~ " + $indyFeedCurrentItem
+# Set background task to loop through remaining RSS feed items
+$Timer = New-Object System.Timers.Timer
+$Timer.Interval = 30000
+$TimerAction = {
+    if ($i -lt ($RSSFeed.Item.Count)) {
+        $IndyFeedCurrentItem = New-RSSFeedCurrentItem $RSSFeed.Item[$i].title
+        $Host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $WeatherString + " ~ " + $IndyFeedCurrentItem
+        $i++
+    } else {
+        $i = 0
+        Remove-Item -Path $PSScriptRoot\RSSFeed.* -Force
+        [xml]$RSS = Invoke-WebRequest -Uri $RSSUri
+        $RSS.rss.channel | Export-Clixml $PSScriptRoot\RSSFeed.xml
+        $RSSFeed = Import-Clixml $PSScriptRoot\RSSFeed.xml
+        $IndyFeedCurrentItem = New-RSSFeedCurrentItem $RSSFeed.Item[$i].title
+        $Host.UI.RawUI.WindowTitle = $PWD.Path + " ~ " + $WeatherString + " ~ " + $IndyFeedCurrentItem
         $i++
     }
 }
-Register-ObjectEvent -InputObject $timer -EventName Elapsed -SourceIdentifier RSSFeed -Action $timerAction | Out-Null
-$timer.Start()
+$ObjectEventProps = @{
+    InputObject = $Timer
+    EventName = "Elapsed"
+    SourceIdentifier = "RSSFeed"
+    Action = $TimerAction
+}
+Register-ObjectEvent @ObjectEventProps | Out-Null
+$Timer.Start()
